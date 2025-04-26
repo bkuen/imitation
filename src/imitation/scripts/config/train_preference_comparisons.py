7fdf3efddc095e1ff308d947fa18d9f38fdc29bb
Benjamin Kuen <info@benjamin-kuen.com>
2025-04-26 10:21:22 +0200

    Fix environment versions

--- src/imitation/scripts/config/train_preference_comparisons.py
+++ src/imitation/scripts/config/train_preference_comparisons.py
@@ -114,7 +114,7 @@ def half_cheetah():
 
 @train_preference_comparisons_ex.named_config
 def seals_half_cheetah():
-    environment = dict(gym_id="seals/HalfCheetah-v0")
+    environment = dict(gym_id="seals/HalfCheetah-v1")
     rl = dict(
         batch_size=512,
         rl_kwargs=dict(
@@ -188,7 +188,7 @@ def seals_swimmer():
 
 @train_preference_comparisons_ex.named_config
 def seals_walker():
-    environment = dict(gym_id="seals/Walker2d-v0")
+    environment = dict(gym_id="seals/Walker2d-v1")
     policy = dict(
         policy_cls="MlpPolicy",
         policy_kwargs=dict(
